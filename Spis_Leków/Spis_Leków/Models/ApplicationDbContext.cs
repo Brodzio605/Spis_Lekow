2f3f3d8de9283c874f28eae3585fef9d9ccbd158
Brodzio605 <p.brodziak1406@gmail.com>
2024-03-29 23:01:30 +0100

    Update ApplicationDbContext.cs

--- Spis_Leków/Spis_Leków/Models/ApplicationDbContext.cs
+++ Spis_Leków/Spis_Leków/Models/ApplicationDbContext.cs
@@ -14,19 +14,6 @@ namespace Spis_Leków.Models
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //if (!optionsBuilder.IsConfigured)
-            //{
-            //    IConfigurationRoot configuration = new ConfigurationBuilder()
-            //       .SetBasePath(Directory.GetCurrentDirectory())
-            //       .AddJsonFile("appsettings.json")
-            //       .Build();
-            //    var connectionString = configuration.GetConnectionString("DefaultConnection");
-            //    optionsBuilder.UseSqlServer(connectionString);
-            //}
-        }
-
         public DbSet<Uzytkownicy> Uzytkownicy { get; set; }
         public DbSet<Formularze> Formularze { get; set; }
         public DbSet<leki> leki { get; set; }
